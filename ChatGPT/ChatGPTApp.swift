05e3ab546d47ae12ee119d25f16e012882ffb7c1
Ayman Jibrael <aymanjibrael@outlook.com>
2023-01-26 16:32:36 +0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Ayman on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
